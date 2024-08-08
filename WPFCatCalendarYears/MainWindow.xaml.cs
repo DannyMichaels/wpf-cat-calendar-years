b9bd250f5177ea22101b4ef5ed27f25a861ab152
DannyMichaels <--replace-all>
2024-08-08 12:56:54 -0400

    add codebehind only

--- WPFCatCalendarYears/MainWindow.xaml.cs
+++ WPFCatCalendarYears/MainWindow.xaml.cs
@@ -16,11 +16,51 @@ namespace WPFCatCalendarYears
     /// </summary>
     public partial class MainWindow : Window
     {
+        public TextBlock txtBlockResult;
+        public TextBox txtCatAge;
+        StackPanel pnlMainVerticalStackPanel;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Image imgBackground = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/Images/cat.jpg")) };
+            imgBackground.Width = 200;
+            imgBackground.Height = 200;
+            txtBlockResult = new TextBlock() { Text = "Your cat is" , FontSize = 18 };
+
+            txtCatAge = new TextBox() 
+            { 
+                Width = 120, 
+                Height = 30, 
+                TextAlignment = TextAlignment.Center, 
+                FontSize = 16,
+                Margin = new Thickness(5, 0, 0, 0)
+            };
+
+            txtCatAge.KeyDown += txtCatAge_KeyDown;
+
+            TextBlock txtBlockUserQuestion = new TextBlock() { Text = "How old is your cat?", FontSize = 18 };
+
+            StackPanel pnlHorizontalStackPanel = new StackPanel() { 
+                Orientation = Orientation.Horizontal, 
+                Margin =  new Thickness(1,5,0,0)
+            };
+
+            pnlHorizontalStackPanel.Children.Add(txtBlockUserQuestion);
+            pnlHorizontalStackPanel.Children.Add(txtCatAge);
+
+            pnlMainVerticalStackPanel = new StackPanel() { Orientation = Orientation.Vertical, HorizontalAlignment = HorizontalAlignment.Left};
+            pnlMainVerticalStackPanel.Children.Add(pnlHorizontalStackPanel);
+            pnlMainVerticalStackPanel.Children.Add(txtBlockResult);
+            pnlMainVerticalStackPanel.Children.Add(imgBackground);
+
+            windowMain.Content = pnlMainVerticalStackPanel;
         }
 
+        /// <summary>
+        /// Handle the enter key pressed on textbox event, calculate the human age of the cat and display the result.
+        /// </summary>
         private void HandleSubmit()
         {
             try
@@ -31,18 +71,26 @@ namespace WPFCatCalendarYears
                 if (iCatAge >= 0 && iCatAge <= 1)
                 {
                     sResultHumanAge = "0-15";
-                    resultTextBlock.Text = "Your cat is " + sResultHumanAge + " years old in human years.";
-                } else if (iCatAge >=2 && iCatAge <25)
+                    txtBlockResult.Text = "Your cat is " + sResultHumanAge + " years old in human years.";
+                }
+                else if (iCatAge >= 2 && iCatAge < 25)
                 {
-                     sResultHumanAge = ((iCatAge - 2) * 4 + 24).ToString();
-                    resultTextBlock.Text = "Your cat is " + sResultHumanAge + " years old in human years.";
-                } else
+                    sResultHumanAge = ((iCatAge - 2) * 4 + 24).ToString();
+                    txtBlockResult.Text = "Your cat is " + sResultHumanAge + " years old in human years.";
+                }
+                else
                 {
-                    resultTextBlock.Text = "You entered a valuie that is not between 0-25. " +
+                    txtBlockResult.Text = "You entered a valuie that is not between 0-25. " +
                                              "Your cat must be super old or not yet born!";
                 }
 
-            } catch (Exception exception)
+                TextBlock txtBlockExtra = new TextBlock() { Text = "Underneath the cat", FontSize = 18 };
+                pnlMainVerticalStackPanel.Children.Add(txtBlockExtra);
+                Button btnDeleteExtra = new Button() { Content = "Delete Extra" };
+                btnDeleteExtra.Click += (sender, e) => pnlMainVerticalStackPanel.Children.Remove(txtBlockExtra);
+                pnlMainVerticalStackPanel.Children.Add(btnDeleteExtra);
+            }
+            catch (Exception exception)
             {
                 MessageBox.Show("Not a valid number, please enter a numberic value! " + exception.Message);
             }
