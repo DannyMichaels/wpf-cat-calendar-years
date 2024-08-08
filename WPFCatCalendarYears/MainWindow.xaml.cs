58440af995e93e99a793a1d945b57cad9d44d932
DannyMichaels <--replace-all>
2024-08-08 11:20:51 -0400

    complete, now make a separate branch without using xaml and only the code behind

--- WPFCatCalendarYears/MainWindow.xaml.cs
+++ WPFCatCalendarYears/MainWindow.xaml.cs
@@ -20,5 +20,39 @@ namespace WPFCatCalendarYears
         {
             InitializeComponent();
         }
+
+        private void HandleSubmit()
+        {
+            try
+            {
+                int iCatAge = Int32.Parse(txtCatAge.Text);
+                string sResultHumanAge = "";
+
+                if (iCatAge >= 0 && iCatAge <= 1)
+                {
+                    sResultHumanAge = "0-15";
+                    resultTextBlock.Text = "Your cat is " + sResultHumanAge + " years old in human years.";
+                } else if (iCatAge >=2 && iCatAge <25)
+                {
+                     sResultHumanAge = ((iCatAge - 2) * 4 + 24).ToString();
+                    resultTextBlock.Text = "Your cat is " + sResultHumanAge + " years old in human years.";
+                } else
+                {
+                    resultTextBlock.Text = "You entered a valuie that is not between 0-25. " +
+                                             "Your cat must be super old or not yet born!";
+                }
+
+            } catch (Exception exception)
+            {
+                MessageBox.Show("Not a valid number, please enter a numberic value! " + exception.Message);
+            }
+        }
+
+        private void txtCatAge_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) {
+                HandleSubmit();
+            }
+        }
     }
 }
